771cc97642643bdd5ef276c530741bb1f36dad9d
Rashem Van Rondina <rashemvanrondina@gmail.com>
2026-04-09 21:25:18 +0800

    Updated 2.5.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,14 +9,3 @@ declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg';
 declare module '*.gif';
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600, // 🟢 Raises the warning limit to 1600kb
-  }
-});
